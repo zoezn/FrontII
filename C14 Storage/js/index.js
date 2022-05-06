810c1f5b9aceb5e5d2ea65def1d2a8e98633b1ca
zxxej <zxejimenez@gmail.com>
2022-05-06 04:10:34 -0300

    mas localstorage besos

--- C14 Storage/js/index.js
+++ C14 Storage/js/index.js
@@ -1,10 +1,48 @@
 /*
 REQUERIMIENTOS
 - utilizar el formulario para captar el texto ingresado
-
-- implmentar el evento "submit", utilizarlo para guardar el comentario en un array
-
+- implementar el evento "submit", utilizarlo para guardar el comentario en un array
 - cada vez que se agrega un nuevo comentario renderizarlo en una etiqueta "p"(sacar del html los hardcodeados y hacerlo dinamico)
-
 - constantemente guardar la informacion en localStorage, si se recarga la pagina deberian mantenerse los comentarios
 */
+
+const formulario = document.forms[0];
+const comentario = document.querySelector("#comentario");
+const boton = document.querySelector("button");
+const comentarios = document.querySelector(".comentarios");
+const arrayComentarios = [];
+
+function hacerComentario(){
+    arrayComentarios.push(comentario.value);
+    localStorage.setItem('comentarios', JSON.stringify(arrayComentarios));
+    agregarComentarioNuevo();
+}
+
+function agregarComentarioNuevo(){
+    comentarios.innerHTML += `<p>${comentario.value}</p>`
+}
+
+function renderizarComentariosViejos(){
+    const comentariosPasados = JSON.parse(localStorage.getItem('comentarios'));
+    for (let i = 0; i < comentariosPasados.length; i++) {
+        arrayComentarios.push(comentariosPasados[i])
+        comentarios.innerHTML += `
+        <p>${comentariosPasados[i]}</p>
+        `
+    }
+}
+
+formulario.addEventListener("submit", e => {
+    e.preventDefault();
+    
+    hacerComentario();
+    formulario.reset();
+
+})
+
+window.addEventListener("load", () => {
+    renderizarComentariosViejos();
+
+})
+
+
